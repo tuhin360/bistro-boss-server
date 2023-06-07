2338912886498ccca3d5b835748fec9cde705d64
tuhin <tuhin153695@gmail.com>
2023-06-07 09:52:28 +0600

    particular item delete added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express()
 const port =  process.env.PORT || 5000;
@@ -46,15 +46,34 @@ async function run() {
     })
 
 
-    // cart collection
+    // cart collection apis
+    app.get('/carts', async(req, res) => {
+      const email = req.query.email;
+      console.log(email);
+      if(!email){
+        res.send([]);
+      }
+      const query = {email: email};
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    })
+
+    //----insert item-----
     app.post('/carts', async(req, res) => {
       const item = req.body;
       console.log(item);
       const result = await cartCollection.insertOne(item);
-      req.send(result);
+      res.send(result)
     })
 
 
+    // delete particular item
+    app.delete('/carts/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id)};
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
+    }) 
 
 
 
