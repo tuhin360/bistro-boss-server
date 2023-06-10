c00c9758da4d7897ac2fe00b2b79eaa1b7ec5fcf
tuhin <tuhin153695@gmail.com>
2023-06-10 14:21:26 +0600

    get all users from mongodb added

--- index.js
+++ index.js
@@ -30,16 +30,46 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+
+    const usersCollection = client.db("bistroDb").collection("users");
     const menuCollection = client.db("bistroDb").collection("menu");
     const reviewCollection = client.db("bistroDb").collection("reviews");
     const cartCollection = client.db("bistroDb").collection("carts");
 
 
+
+    //users related apis
+    
+    // Getting all users from DB
+    app.get('/users', async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    })
+
+
+
+    app.post('/users', async(req, res) => {
+      const user = req.body;
+      // console.log(user);
+      const query = {email: user.email}
+      const existingUser = await usersCollection.findOne(query);
+      // console.log('Existing user', existingUser);
+      if(existingUser){
+        return res.send({message: 'user already exists'})
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    })
+
+
+
+    //menu related apis
     app.get('/menu', async(req, res) => {
         const result = await menuCollection.find().toArray();
         res.send(result)
     })
 
+    //review related apis
     app.get('/reviews', async(req, res) => {
         const result = await reviewCollection.find().toArray();
         res.send(result)
@@ -61,7 +91,6 @@ async function run() {
     //----insert item-----
     app.post('/carts', async(req, res) => {
       const item = req.body;
-      console.log(item);
       const result = await cartCollection.insertOne(item);
       res.send(result)
     })
