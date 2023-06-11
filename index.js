92ba621d152aaea2d446d6cd0902514b6ce17e5c
tuhin <tuhin153695@gmail.com>
2023-06-11 17:42:19 +0600

    delete menu item added

--- index.js
+++ index.js
@@ -147,6 +147,14 @@ async function run() {
       const newItem = req.body;
       const result = await menuCollection.insertOne(newItem);
       res.send(result);
+    });
+
+    // delete item 
+    app.delete('/menu/:id', verifyJWT, verifyAdmin, async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await menuCollection.deleteOne(query);
+      res.send(result);
     })
 
 
